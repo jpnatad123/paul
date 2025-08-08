f9b6b674cc7cd30e94f1f0dc7f273e808c022fff
jpnatad123 <jpnatad123@gmail.com>
2025-08-08 11:00:16 +0800

    I added Banksystem

---
+++ src/BankingSystem/BankingClass.java
@@ -0,0 +1,10 @@
+package BankingSystem;
+
+public class BankingClass {
+    private final int storedAccountNo = 123123;
+    private final int storedPin = 137982;
+
+    public boolean verifyAccount(int accountNo, int pin) {
+        return accountNo == storedAccountNo && pin == storedPin;
+    }
+}
